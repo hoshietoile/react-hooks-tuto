f7ec55635926b6444104c5c81deca6671d364eda
Yuta <francekakumei714@gmail.com>
2021-04-18 11:43:44 +0900

    環境関連

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+// https://harkerhack.com/vscode-react-eslint-prettier/
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parser: 'babel-eslint',
+  plugins: ['react', 'prettier'],
+  parserOptions: {
+    version: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended', 'prettier/react'],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+}
